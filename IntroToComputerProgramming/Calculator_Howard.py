5fef7a99f801b51f0f8f3152576a54a1eda0fcc8
Emily Howard <ehoward2@mail.umw.edu>
2019-02-04 21:24:57 -0500

    Assignment 1.1

---
+++ IntroToComputerProgramming/Calculator_Howard.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 11:46:34 2019
+
+@author: emilyhoward
+"""
+
+x=3
+y=2
+z=4
+mylist = []
+r=x+y
+mylist.append(1)
+s=y*z+3*x
+mylist.append(2)
+d=r**2
+mylist.append(3)
+h= (2*s-x/2)/r
+mylist.append(4)
+k= 7%3
+mylist.append(5)
+t=3+d
+mylist.append(3)
+e=k*(3/4)
+mylist.append(5)
